8d9e5860df20cc4af85f6ebaa80dfa035cf56ed9
gastoncarrizo12 <116860374+gastoncarrizo12@users.noreply.github.com>
2023-06-12 16:09:55 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Navbar, Footer} from './pages/layout';
+import { Home } from './pages/inicio';
+import { Tutoriales } from './pages/tutorial';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App () {
+  return(
+    <>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/tutorial' element={<Tutoriales />} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+   
+    </>
+      
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
